bd1530f3fe11d3ed1d81d2591c8cd6429edc3e39
Ha1f_Deaf <jaredtspears@gmail.com>
2018-09-04 20:16:51 -0400

    working on js file framework, main function for renderArrivals is not working just yet.

--- assets/Moment.js
+++ assets/Moment.js
@@ -14,6 +14,11 @@ const Arrivals = {
     },
     //trying to break into minutes figured each hr is 60 min so it would be a 1/60th of an hr
     frequency: 1%60,
+    status: {
+        //not sure how to arrainge the ontime or delayed notice
+        onTime: " On Time ",
+        delayed: " Delayed "
+    }
 }
 const Departures = {
     name: [],//will be added by admin
@@ -30,38 +35,50 @@ const Departures = {
     },
     //trying to break into minutes figured each hr is 60 min so it would be a 1/60th of an hr
     frequency: 1%60,
+    status: {
+        //not sure how to arrainge the ontime or delayed notice
+        ontime: " On Time ",
+        delayed: " Delayed "
+    }
 }
-
-//bare bone call to the above Arrival or Departures create [object:object]
-// if consologing into the object like line 37 it works and I can see the array
-console.log("This is the Arrivals Object" + Arrivals.firstTrainTime.am);
-console.log("This is the Departures Object" + Departures);
+    //bare bone call to the above Arrival or Departures create [object:object]
+    // if consologing into the object like line 37 it works and I can see the array
+    console.log("This is the Arrivals Object: " + Arrivals.firstTrainTime.am);
+    console.log("This is the Departures Object: " + Departures);
 
 //Arrivals Main function
 function renderArrivals(){
     //will run through object of Arrivals by either name, destination or first_train_time
-   //not sure how to create the object var
-    var object = Arrivals[i];
-    for(Arrivals in object){
-        if(object.hasOwnProperty(name)){
+    for(var key in Arrivals){
+        if(Arrivals.hasOwnProperty(name)){
             //create a name peramiter (have an open array)
 
-            console.log(name)
-        } else if(object.hasOwnProperty(destination)){
+            console.log("this should be the name array for arrivals " + name[key]);
+        } else if(Arrivals.hasOwnProperty(aDestination)){
             //create a destination peramiter (have an open array)
-        } else if(object.hasOwnProperty(first_train_time.am)){
+        } else if(Arrivals.hasOwnProperty(first_train_time.am)){
             //creating a loop to run and catch am times (MIGHT NOT NEED)
             for(i=0;i>11; i++){
             $("#live_updates_Arrivals").text(first_train_time.am + "AM")
             console.log(first_train_time.am);
             }
-        } else if(object.hasOwnProperty(first_train_time.pm)){
+        } else if(Arrivals.hasOwnProperty(first_train_time.pm)){
             //creating a loop to run and catch pm times (MIGHT NOT NEED)
             for(i=0;i>11; i++){
 
+            }
+        }else if(Arrivals.hasOwnProperty(status)){
+            if(onTime){
+                //making var ticketHolderStatus equal to odd times only
+                var ticketHoldersStatus = Arrivals.firstTrainTime.am["1","3","5","7","9","11"] ||
+                                          Arrivals.firstTrainTime.pm["14","15","17", "19", "21", "23"];
+                    console.log(ticketHoldersStatus);
+                FirstTrainTime != ticketHoldersStatus;
+                     console.log(onTime);
             }
         }
     }
 }
 
 renderArrivals();
+console.log(renderArrivals);
